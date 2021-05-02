10878b36f2b887ee34d975197b2984b0cd80eb1c
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-05-02 06:01:36 +0600

    undo

--- design_chapter_06/src/com/obydul/undo/RemoteControlWithUndo.java
+++ design_chapter_06/src/com/obydul/undo/RemoteControlWithUndo.java
@@ -4,11 +4,11 @@ public class RemoteControlWithUndo {
 	Command[] onCommands;
 	Command[] offCommands;
 	Command undoCommand;
-	
+
 	public RemoteControlWithUndo() {
 		onCommands = new Command[7];
 		offCommands = new Command[7];
-		
+
 		Command noCommand = new NoCommand();
 		for (int i = 0; i < 7; i++) {
 			onCommands[i] = noCommand;
@@ -16,19 +16,36 @@ public class RemoteControlWithUndo {
 		}
 		undoCommand = noCommand;
 	}
-	
-	public void setCommand(int slot, Command onCommand, Command offCommand){
+
+	public void setCommand(int slot, Command onCommand, Command offCommand) {
 		onCommands[slot] = onCommand;
 		offCommands[slot] = offCommand;
 	}
-	
-	public void onButtonWasPushed(int slot){
+
+	public void onButtonWasPushed(int slot) {
 		onCommands[slot].execute();
 		undoCommand = offCommands[slot];
 	}
-	public void offButtonWasPushed(int slot){
+
+	public void offButtonWasPushed(int slot) {
 		offCommands[slot].execute();
 		undoCommand = offCommands[slot];
+	}
+
+	public void undoButtonWasPushed() {
+		undoCommand.undo();
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("\n---- Remote Control ----\n");
+		for (int i = 0; i < onCommands.length; i++) {
+			sb.append("[slot " + i + "] " + onCommands[i].getClass().getName()
+					+ "    " + offCommands[i].getClass().getName() + "\n");
+		}
+		sb.append("[undo] " + undoCommand.getClass().getName() + "\n");
+		return sb.toString();
 	}
 
 }
