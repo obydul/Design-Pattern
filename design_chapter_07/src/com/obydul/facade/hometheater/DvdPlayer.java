cb87a685f4eec7edbf7b49c314b31c12786f3a0e
BTS-2 <BTS-2@DESKTOP-4KCAUJN>
2021-05-03 10:12:59 +0600

    Facade Example Done

--- design_chapter_07/src/com/obydul/facade/hometheater/DvdPlayer.java
+++ design_chapter_07/src/com/obydul/facade/hometheater/DvdPlayer.java
@@ -5,25 +5,25 @@ public class DvdPlayer {
 	int currentTrack;
 	Amplifier amplifier;
 	String movie;
-	
+
 	public DvdPlayer(String description, Amplifier amplifier) {
 		this.description = description;
 		this.amplifier = amplifier;
 	}
- 
+
 	public void on() {
 		System.out.println(description + " on");
 	}
- 
+
 	public void off() {
 		System.out.println(description + " off");
 	}
 
-        public void eject() {
+	public void eject() {
 		movie = null;
-                System.out.println(description + " eject");
-        }
- 
+		System.out.println(description + " eject");
+	}
+
 	public void play(String movie) {
 		this.movie = movie;
 		currentTrack = 0;
@@ -32,10 +32,12 @@ public class DvdPlayer {
 
 	public void play(int track) {
 		if (movie == null) {
-			System.out.println(description + " can't play track " + track + " no dvd inserted");
+			System.out.println(description + " can't play track " + track
+					+ " no dvd inserted");
 		} else {
 			currentTrack = track;
-			System.out.println(description + " playing track " + currentTrack + " of \"" + movie + "\"");
+			System.out.println(description + " playing track " + currentTrack
+					+ " of \"" + movie + "\"");
 		}
 	}
 
@@ -43,7 +45,7 @@ public class DvdPlayer {
 		currentTrack = 0;
 		System.out.println(description + " stopped \"" + movie + "\"");
 	}
- 
+
 	public void pause() {
 		System.out.println(description + " paused \"" + movie + "\"");
 	}
@@ -51,11 +53,11 @@ public class DvdPlayer {
 	public void setTwoChannelAudio() {
 		System.out.println(description + " set two channel audio");
 	}
- 
+
 	public void setSurroundAudio() {
 		System.out.println(description + " set surround audio");
 	}
- 
+
 	public String toString() {
 		return description;
 	}
