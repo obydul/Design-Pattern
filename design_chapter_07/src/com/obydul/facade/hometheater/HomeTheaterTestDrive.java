057aec6c8f8d36746ac8cf5a6f9b9a1cbe315dec
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-05-03 19:49:41 +0600

    Home Theater

--- design_chapter_07/src/com/obydul/facade/hometheater/HomeTheaterTestDrive.java
+++ design_chapter_07/src/com/obydul/facade/hometheater/HomeTheaterTestDrive.java
@@ -10,11 +10,10 @@ public class HomeTheaterTestDrive {
 		TheaterLights lights = new TheaterLights("Theater Ceiling Lights");
 		Screen screen = new Screen("Theater Screen");
 		PopcornPopper popper = new PopcornPopper("Popcorn Popper");
- 
-		HomeTheaterFacade homeTheater = 
-				new HomeTheaterFacade(amp, tuner, dvd, cd, 
-						projector, screen, lights, popper);
- 
+
+		HomeTheaterFacade homeTheater = new HomeTheaterFacade(amp, tuner, dvd,
+				cd, projector, screen, lights, popper);
+
 		homeTheater.watchMovie("Raiders of the Lost Ark");
 		homeTheater.endMovie();
 	}
