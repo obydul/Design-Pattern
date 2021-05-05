fa70975e9273d3950316b0c6c0ddec9693143481
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-05-05 09:01:57 +0600

    iterator dinermergeri

---
+++ design_chapter_09/src/com/obydul/iterator/dinermergeri/Menu.java
@@ -0,0 +1,8 @@
+package com.obydul.iterator.dinermergeri;
+
+import java.util.Iterator;
+
+public interface Menu {
+	public Iterator createIterator();
+
+}
---
+++ design_chapter_09/src/com/obydul/iterator/dinermergeri/MenuTestDrive.java
@@ -0,0 +1,27 @@
+package com.obydul.iterator.dinermergeri;
+
+public class MenuTestDrive {
+	public static void main(String[] args) {
+		PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+		DinerMenu dinerMenu = new DinerMenu();
+		Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+		waitress.printMenu();
+		waitress.printVegetarianMenu();
+
+		System.out.println("\nCustomer asks, is the Hogdog vegetarian?");
+		System.out.println("Waitress says: ");
+		if (waitress.isItemVegetarian("Hotdog")) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+		System.out.println("\nCustomer asks, are the Waffles vegetarian?");
+		System.out.println("Waitress says: ");
+		if (waitress.isItemVegetarian("Waffles")) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	}
+
+}
