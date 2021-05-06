9629af89f34d08447659684fabfd1ad24dde5ed2
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-05-06 09:51:29 +0600

    iterator dinermerger

---
+++ design_chapter_09/src/com/obydul/iterator/dinermerger/MenuTestDrive.java
@@ -0,0 +1,11 @@
+package com.obydul.iterator.dinermerger;
+
+public class MenuTestDrive {
+	public static void main(String[] args) {
+		PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+		DinerMenu dinerMenu = new DinerMenu();
+		
+		Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+		waitress.printMenu();
+	}
+}
